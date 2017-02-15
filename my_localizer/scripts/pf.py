897dc0942a570ce2440ce2dcca6a0d17d9684c57
nathanyee <nathaniel.yee@students.olin.edu>
2017-02-14 20:53:11 -0500

    added ros service call comments

--- my_localizer/scripts/pf.py
+++ my_localizer/scripts/pf.py
@@ -123,6 +123,11 @@ class ParticleFilter:
         # request the map from the map server, the map should be of type nav_msgs/OccupancyGrid
         # TODO: fill in the appropriate service call here.  The resultant map should be assigned be passed
         #       into the init method for OccupancyField
+
+
+        # Make a ros service call to the /static_map service to get a nav_msgs/OccupancyGrid map.
+        # Then use OccupancyField to make the map object 
+
         robotMap = rospy.ServiceProxy('/static_map', GetMap)().map
         self.occupancy_field = OccupancyField(robotMap)
         print "OccupancyField initialized", self.occupancy_field
